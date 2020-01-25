20818c824939a3f203827af2e018e8cb74ed88e3
Chris Patrick <atomize101@gmail.com>
2020-01-24 23:15:42 -0500

    Creating the Canvas

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hi there');
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const engine = Engine.create();
+const { world } = engine;
+
+const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: 800,
+        height: 600
+    }
+});
+
+Render.run(render);
+Runner.run(Runner.create(), engine);
