b9e26a3b0c8a39f0d714d0583d3920a6c3435c4c
Chris Patrick <atomize101@gmail.com>
2020-02-06 10:14:37 -0500

    Adding in random circle and square shapes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
+const width = 800; 
+const height = 600; 
+
 const engine = Engine.create();
 const { world } = engine;
 
@@ -7,8 +10,9 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
+        wireframes: false,
+        width: width,
+        height: height
     }
 });
 
@@ -39,4 +43,12 @@ const walls = [
 
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// Random Shapes, squares and circles
+
+for(let i = 0; i < 35; i++) {
+    if(Math.random() > 0.5) {
+        World.add(world, Bodies.rectangle(Math.random() * width ,Math.random() * height, 50, 50));               
+    } else {
+        World.add(world, Bodies.circle(Math.random() * width ,Math.random() * height, 35));
+    }
+}
