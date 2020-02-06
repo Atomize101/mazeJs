8649d54038d74787968b7c94c5cabdd240d26fc7
Chris Patrick <atomize101@gmail.com>
2020-02-06 10:54:03 -0500

    Starting to build maze grid using for loop

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800; 
+const width = 600; 
 const height = 600; 
 
 const engine = Engine.create();
@@ -10,7 +10,7 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        wireframes: false,
+        wireframes: true,
         width: width,
         height: height
     }
@@ -19,36 +19,33 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-    world, 
-    MouseConstraint.create(engine, { 
-    mouse: Mouse.create(render.canvas)
-}));
-
 // Walls
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, {
+    Bodies.rectangle(width / 2, 0, width, 40, {
         isStatic: true
     }),
-    Bodies.rectangle(400, 600, 800, 40, {
+    Bodies.rectangle(width / 2, height, width, 40, {
         isStatic: true
     }),
-    Bodies.rectangle(0, 300, 40, 600, {
+    Bodies.rectangle(0, height / 2, 40, height, {
         isStatic: true
     }),
-    Bodies.rectangle(800, 300, 40, 600, {
+    Bodies.rectangle(width, height / 2, 40, height, {
         isStatic: true
     })
 ];
 
 World.add(world, walls);
 
-// Random Shapes, squares and circles
+// Maze generation
 
-for(let i = 0; i < 35; i++) {
-    if(Math.random() > 0.5) {
-        World.add(world, Bodies.rectangle(Math.random() * width ,Math.random() * height, 50, 50));               
-    } else {
-        World.add(world, Bodies.circle(Math.random() * width ,Math.random() * height, 35));
+const grid = [];
+
+for(let i = 0; i < 3; i++) {
+    grid.push( [] );
+    for (let j = 0; j < 3; j++) {
+        grid[i].push(false);
     }
 }
+
+console.log(grid)
