846b4e0e002460250eda2800fe9b599797463dbf
Chris Patrick <atomize101@gmail.com>
2020-02-06 11:20:25 -0500

    Adding Horizontal and Vertical arrays

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3;
 const width = 600; 
 const height = 600; 
 
@@ -39,9 +40,19 @@ World.add(world, walls);
 
 // Maze generation
 
-const grid = Array(3)
+const grid = Array(cells)
 .fill(null)
-.map(() => Array(3).fill(false));
+.map(() => Array(cells).fill(false));
 
+const verticals = Array(cells)
+    .fill(null)
+    .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells -1)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+console.log(horizontals);
+console.log(verticals);
 console.log(grid);
 
